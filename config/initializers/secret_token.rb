23e18cd638444688c07ca5f787137d1fc92c1f4d
Wesley Zhao <wesley.zhao@gmail.com>
2014-01-15 23:00:46 -0500

    rails app first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cis196Hw1::Application.config.secret_key_base = 'a4b308a361a34b7879ad6a66eeae6cdc5487119b001d195d963ea05d6ecdb7f9f8d21a4ab9672ea57b19bf9f7fbe0269edf80d438beb8de00e63e594294cd841'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cis196Hw1::Application.config.session_store :cookie_store, key: '_cis196_hw1_session'
