106cdfe0ad930eb6f728878edde89d92e3b70d6a
Wesley Zhao <wesley.zhao@gmail.com>
2014-01-15 23:29:01 -0500

    created basic welcome page for production

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Cis196Hw1::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
